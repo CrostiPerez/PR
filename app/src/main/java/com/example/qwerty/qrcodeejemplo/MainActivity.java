68ee977148bb9fbe89cceb50f68534fd356286c2
Armando Serna Gutiérrez <armandoserna97@gmail.com>
2018-03-09 19:21:27 -0600

    Commit inicial
    Activity principal manda un petición de código a la activity para scanear y esta devuelve devuelve un resultado del código leído.
    La activity principal escribe el resultado en un TextView.

---
+++ app/src/main/java/com/example/qwerty/qrcodeejemplo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.qwerty.qrcodeejemplo;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnScan;
+    TextView txtResult;
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST= 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnScan = (Button)findViewById(R.id.btnScan);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+        btnScan.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+               startActivityForResult(intent, REQUEST_CODE);
+           }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                final Barcode barcode =data.getParcelableExtra("barcode");
+                txtResult.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtResult.setText(barcode.displayValue);
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/qwerty/qrcodeejemplo/ScanActivity.java
@@ -0,0 +1,88 @@
+package com.example.qwerty.qrcodeejemplo;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.util.SparseBooleanArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanActivity extends AppCompatActivity {
+    SurfaceView cameraView;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    SurfaceHolder surfaceHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        cameraView = (SurfaceView) findViewById(R.id.cameraView);
+        cameraView.setZOrderMediaOverlay(true);
+        surfaceHolder = cameraView.getHolder();
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        if (!barcodeDetector.isOperational()) {
+            Toast.makeText(getApplicationContext(), "Sorry, couldn't setup that detector", Toast.LENGTH_SHORT).show();
+            this.finish();
+        }
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(24)
+                .setAutoFocusEnabled(true)
+                .setRequestedPreviewSize(1920, 1024)
+                .build();
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ContextCompat.checkSelfPermission(ScanActivity.this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(cameraView.getHolder());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() > 0) {
+                    Intent intent = new Intent();
+                    intent.putExtra("barcode", barcodes.valueAt(0));
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
