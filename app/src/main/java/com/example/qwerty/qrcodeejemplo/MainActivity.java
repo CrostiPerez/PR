df88e137c93dfd28ada6cfbdf92566a422381173
Sergio Pérez <ropealse@hotmail.com>
2018-04-07 13:26:33 -0500

    PDF View

--- app/src/main/java/com/example/qwerty/qrcodeejemplo/MainActivity.java
+++ app/src/main/java/com/example/qwerty/qrcodeejemplo/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.barcode.Barcode;
@@ -22,15 +21,20 @@ import org.json.JSONException;
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnScan;
+    Button btnScan, btnCons;
     public static final int REQUEST_CODE = 100;
-    public static final int PERMISSION_REQUEST= 200;
+    public static final int PERMISSION_REQUEST = 200;
+    public static final int CHRONOMETER = 300;
+    public static final int PDF = 400;
+    int type = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnScan = findViewById(R.id.btnProceso);
-
+        btnCons = findViewById(R.id.btnConsulta);
+        type = 0;
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, PERMISSION_REQUEST);
         }
@@ -39,20 +43,33 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v) {
                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
                startActivityForResult(intent, REQUEST_CODE);
+               type = 300;
            }
+        });
+        btnCons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+                type = 400;
+            }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && type == CHRONOMETER) {
             if (data != null) {
                 final Barcode barcode =data.getParcelableExtra("barcode");
                 RequestParams params = new RequestParams();
                 params.put("id", barcode.displayValue);
                 doSomeNetworking(params);
             }
+        }
+        else if(requestCode == REQUEST_CODE && resultCode == RESULT_OK && type == PDF){
+            Intent intent = new Intent(MainActivity.this, PDFActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
         }
     }
 
---
+++ app/src/main/java/com/example/qwerty/qrcodeejemplo/PDFActivity.java
@@ -0,0 +1,53 @@
+package com.example.qwerty.qrcodeejemplo;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PDFActivity extends AppCompatActivity {
+    PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+
+        pdfView = findViewById(R.id.pdf);
+        //pdfView.fromAsset("CCA-Certificate-Java Level 1.pdf").load();
+        new RetrievePDFStream().execute("https://www.rstudio.com/wp-content/uploads/2015/03/rmarkdown-spanish.pdf");
+    }
+
+   @SuppressLint("StaticFieldLeak")
+   class RetrievePDFStream extends AsyncTask <String, Void, InputStream>{
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                if(urlConnection.getResponseCode() == 200){
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+            }
+            catch (IOException e) {
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream).load();
+        }
+    }
+}
